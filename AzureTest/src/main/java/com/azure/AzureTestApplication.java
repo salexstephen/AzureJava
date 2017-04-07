4faaa5b2cb5639444ec3d3244e88b635635dfa09
alex <salexstephen@gmail.com>
2017-04-07 16:50:43 +0530

    azure code

---
+++ AzureTest/src/main/java/com/azure/AzureTestApplication.java
@@ -0,0 +1,12 @@
+package com.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureTestApplication.class, args);
+	}
+}
---
+++ AzureTest/src/test/java/com/azure/AzureTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.azure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AzureTestApplication.class)
+@WebAppConfiguration
+public class AzureTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
